267209c122801df8f3d66e31317a80aebc781f51
André Silva <andre06x@gmail.com>
2024-01-23 21:09:34 -0300

    adicionando modulos

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { RedisModule } from './redis.module';
+import { RedisController } from './redis.controller';
 import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [EventsModule],
-  controllers: [AppController],
+  imports: [EventsModule, RedisModule],
+  controllers: [AppController, RedisController],
   providers: [AppService],
 })
 export class AppModule {}
