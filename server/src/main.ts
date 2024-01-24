1250e4a7d1aa5e84475bac5774f430403a91c465
André Silva <andre06x@gmail.com>
2024-01-23 21:10:17 -0300

    add cors trpc

--- server/src/main.ts
+++ server/src/main.ts
@@ -3,9 +3,12 @@ import { AppModule } from './app.module';
 import { appRouter, createContext } from './helloWorld';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import { IoAdapter } from '@nestjs/platform-socket.io';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+
   app.use(
     '/trpc',
     trpcExpress.createExpressMiddleware({
