571761941934907102310b6656add9ef3a07b9fe
André Silva <andre06x@gmail.com>
2024-01-25 20:00:33 -0300

    add room controller

---
+++ server/src/controllers/room.controller.ts
@@ -0,0 +1,54 @@
+// messages/messages.controller.ts
+import { Controller, Post, Get, Param, Body } from '@nestjs/common';
+import { RoomService } from 'src/services/room.service';
+
+@Controller('room')
+export class RoomController {
+  constructor(private readonly roomService: RoomService) {}
+
+  @Post()
+  async createRoom(
+    @Body('name') name: string,
+    @Body('userAdmId') userAdmId: string,
+  ) {
+    return this.roomService.createRoom(name, userAdmId);
+  }
+
+  @Post('last-messages')
+  async lastMessages(@Body('id') id: string) {
+    return this.roomService.lastMessages(id);
+  }
+
+  @Post('messages')
+  async sendMenssage(
+    @Body('id') id: string,
+    @Body('content') content: string,
+    @Body('idRoom') idRoom: string,
+  ) {
+    return this.roomService.sendMenssage(content, id, idRoom);
+  }
+
+  @Post('request-room')
+  async requestRoom(@Body('id') id: string, @Body('idRoom') idRoom: string) {
+    return this.roomService.requestRoom(id, idRoom);
+  }
+
+  @Post('list-request-room')
+  async listRequests(@Body('id') id: string, @Body('idRoom') idRoom: string) {
+    return this.roomService.listRequests(id, idRoom);
+  }
+
+  @Post('update-request')
+  async updatedRequest(
+    @Body('id') id: string,
+    @Body('idRoom') idRoom: string,
+    @Body('idAprove') idAprove: string,
+  ) {
+    return this.roomService.updatedRequest(id, idRoom, idAprove);
+  }
+  /*
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.roomService.findOne(id);
+  }*/
+}
