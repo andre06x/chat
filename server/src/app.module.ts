5c0a8ed8b8d4f48611eb08c52d83ba788a627541
André Silva <andre06x@gmail.com>
2024-01-25 20:00:23 -0300

    add modules mongodb, controller, service

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,13 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { RedisModule } from './redis.module';
-import { RedisController } from './redis.controller';
+
 import { EventsModule } from './events/events.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RoomSchema, UserSchema } from './schema/schema';
+import { UserController } from './controllers/user.controller';
+import { UserService } from './services/user.service';
+import { RoomController } from './controllers/room.controller';
+import { RoomService } from './services/room.service';
 
 @Module({
-  imports: [EventsModule, RedisModule],
-  controllers: [AppController, RedisController],
-  providers: [AppService],
+  imports: [
+    EventsModule,
+    MongooseModule.forRoot('mongodb://localhost/chat'),
+    MongooseModule.forFeature([
+      { name: 'User', schema: UserSchema },
+      { name: 'Room', schema: RoomSchema },
+    ]),
+  ],
+  controllers: [AppController, UserController, RoomController],
+  providers: [AppService, UserService, RoomService],
 })
 export class AppModule {}
