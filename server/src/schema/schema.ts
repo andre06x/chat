a902c3af7fd576ea780830edaee4c451b17650c1
André Silva <andre06x@gmail.com>
2024-01-25 20:01:02 -0300

    add schema mongoose

---
+++ server/src/schema/schema.ts
@@ -0,0 +1,45 @@
+import * as mongoose from 'mongoose';
+import { boolean } from 'zod';
+
+export const UserSchema = new mongoose.Schema({
+  email: String,
+  name: String,
+  password: String,
+});
+
+export const RoomSchema = new mongoose.Schema({
+  userAdmId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  name: String,
+  messages: [
+    {
+      content: String,
+      _id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+      name: String,
+      timestamp: { type: Date, default: Date.now },
+    },
+  ],
+  requests: [
+    {
+      user: {
+        _id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+        name: String,
+        email: String,
+      },
+      status: {
+        type: Boolean,
+        required: false,
+        default: true,
+      },
+      show: {
+        type: Boolean,
+        required: false,
+        default: false,
+      },
+
+      date: {
+        type: Date,
+        default: Date.now,
+      },
+    },
+  ],
+});
