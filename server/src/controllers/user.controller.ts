0e6553a794c213af1faac1050eb4c26eefefb8ef
André Silva <andre06x@gmail.com>
2024-01-25 20:00:45 -0300

    add user controller

---
+++ server/src/controllers/user.controller.ts
@@ -0,0 +1,27 @@
+// messages/messages.controller.ts
+import { Controller, Post, Get, Param, Body } from '@nestjs/common';
+import { UserService } from 'src/services/user.service';
+
+@Controller('user')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Post()
+  async createUser(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Body('name') name: string,
+  ) {
+    return this.userService.createUser(email, password, name);
+  }
+
+  @Get()
+  async getAllUser() {
+    return this.userService.getAllUser();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) {
+    return this.userService.findOne(id);
+  }
+}
