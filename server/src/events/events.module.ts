91ae1540aea88632517ab7b39ba91ce453885a77
André Silva <andre06x@gmail.com>
2024-01-29 21:43:56 -0300

    adicionando imports providers

--- server/src/events/events.module.ts
+++ server/src/events/events.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
 import { EventsGateway } from './events.gateway';
+import { RoomService } from 'src/services/room.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { RoomSchema, UserSchema } from 'src/schema/schema';
 
 @Module({
-  providers: [EventsGateway],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/chat'),
+    MongooseModule.forFeature([
+      { name: 'User', schema: UserSchema },
+      { name: 'Room', schema: RoomSchema },
+    ]),
+  ],
+  providers: [EventsGateway, RoomService],
 })
 export class EventsModule {}
